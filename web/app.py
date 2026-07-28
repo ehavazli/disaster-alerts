f5bc23969cdfda092c4abb04c99d6dfd641ff8e6
Emre Havazli <emre.havazli@jpl.nasa.gov>
2026-07-28 12:08:23 -0700

    Restore linked OSM/CARTO attribution in Leaflet viewer to match Folium map

--- web/app.py
+++ web/app.py
@@ -752,7 +752,11 @@ def show_maps():
                 var streetMap = L.tileLayer(
                     'https://{{s}}.basemaps.cartocdn.com/light_all/{{z}}/{{x}}/{{y}}{{r}}.png',
                     {{
-                        attribution: '&copy; OpenStreetMap contributors &copy; CARTO',
+                        attribution:
+                            '&copy; <a href="https://www.openstreetmap.org/'
+                            + 'copyright">OpenStreetMap</a> contributors '
+                            + '&copy; <a href="https://carto.com/'
+                            + 'attributions">CARTO</a>',
                         maxZoom: 20
                     }}
                 );
